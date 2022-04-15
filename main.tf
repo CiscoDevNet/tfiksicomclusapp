899ee2a901dcd9aadad7851b58863f7e828b9d70
prathjan <prathjan@cisco.com>
2022-04-15 11:06:46 -0700

    host decode

--- main.tf
+++ main.tf
@@ -29,10 +29,11 @@ resource helm_release helloiksfrtfcb {
 
 provider "helm" {
   kubernetes {
-    host = local.kube_config.clusters[0].cluster.server
-    client_certificate = local.kube_config.users[0].user.client-certificate-data
-    client_key = local.kube_config.users[0].user.client-key-data
-    cluster_ca_certificate = local.kube_config.clusters[0].cluster.certificate-authority-data
+    #host = local.kube_config.clusters[0].cluster.server
+    host = local.host
+    client_certificate = base64decode(local.kube_config.users[0].user.client-certificate-data)
+    client_key = base64decode(local.kube_config.users[0].user.client-key-data)
+    cluster_ca_certificate = base64decode(local.kube_config.clusters[0].cluster.certificate-authority-data)
   }
 }
 
