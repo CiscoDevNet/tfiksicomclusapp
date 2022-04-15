cbc3f3064e9e552190abb9bea2efbbd9324f817e
prathjan <prathjan@cisco.com>
2022-04-15 11:20:14 -0700

    no comments

--- main.tf
+++ main.tf
@@ -29,7 +29,6 @@ resource helm_release helloiksfrtfcb {
 
 provider "helm" {
   kubernetes {
-    #host = local.kube_config.clusters[0].cluster.server
     host = local.host
     client_certificate = base64decode(local.kube_config.users[0].user.client-certificate-data)
     client_key = base64decode(local.kube_config.users[0].user.client-key-data)
@@ -43,9 +42,6 @@ output "host" {
 
 locals {
   kube_config = yamldecode(base64decode(data.terraform_remote_state.iksws.outputs.kube_config))
-    host = yamldecode(base64decode(data.terraform_remote_state.iksws.outputs.kube_config)).clusters[0].cluster.server
-#    client_certificate = local.kube_config.users[0].user.client-certificate-data
-#    client_key = local.kube_config.users[0].user.client-key-data
-#    cluster_ca_certificate = local.kube_config.clusters[0].cluster.certificate-authority-data
+  host = yamldecode(base64decode(data.terraform_remote_state.iksws.outputs.kube_config)).clusters[0].cluster.server
 }
 
